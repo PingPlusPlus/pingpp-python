a62d724f0594f30e1be8b957b6a0e8d554be58aa
Lekton <takato3000@gmail.com>
2014-12-30 13:36:28 +0800

    Pingpp-Python 2.0.0
    pingplusplus.com to pingxx.com

--- example/withflask.py
+++ example/withflask.py
@@ -1,17 +1,34 @@
 __author__ = 'Lekton'
 
 import pingpp
-from flask import Flask,request
+from flask import Flask,request,Response
+import json
+import random
+import string
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/pay', methods=['POST'])
 def do_charge():
+    print request.url
     form = request.get_json()
-    pingpp.api_key = "sk_test_G4W184yvjXbLOq5SS8LWjHyP"
-    response_charge = pingpp.Charge.create(api_key="sk_test_G4W184yvjXbLOq5SS8LWjHyP", **form)
-    return str(response_charge)
+    print form
+    salt = ''.join(random.sample(string.ascii_letters + string.digits, 8))
+    if isinstance(form, dict):
+        form['order_no'] = salt
+        form['app'] = dict(id="app_id")
+        form['currency'] = "cny"
+        form['client_ip'] = "192.168.1.1"
+        form['subject'] = "test-subject"
+        form['body'] = "test-body"
+    print form
+    pingpp.api_key = "sk_live_1SujP4z9mLq54CKqbDqvDqH8"
+    response_charge = pingpp.Charge.create(api_key="key", **form)
+    print "Response_Charge: " + str(response_charge)
+    return Response(json.dumps(response_charge), mimetype='application/json')
+
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,port=8888,host="0.0.0.0")
